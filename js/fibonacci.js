56bf6c6715c65ef96d2870a3c48ba9276932b6d1
ksahinbedirhan <ksahinbedirhan@gmail.com>
2023-05-13 14:53:18 +0300

    x

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,6 +1,6 @@
 let n = parseInt(prompt("Kaç adet Fibonacci sayısı istiyorsun?"));
 let fibonacci = [0, 1];
-for(let i=2; i<n; i++){
-    fibonacci
+for (let i=2; i <=n; i++){
+    fibonacci.push(fibonacci[i-1]+fibonacci[i-2]);
 }
-
+console.log(fibonacci);
