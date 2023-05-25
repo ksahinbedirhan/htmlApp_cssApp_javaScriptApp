ef7f4c4d5dd75c6dcb21018b3f793a14d1e5565a
ksahinbedirhan <ksahinbedirhan@gmail.com>
2023-05-25 17:50:36 +0300

    x

--- js/DOM.js
+++ js/DOM.js
@@ -1,11 +1,30 @@
-console.log(document);
+// console.log(document);
 
-console.log(document.location);
+// console.log(document.location);
 
-console.log(document.location.pathname);
+// console.log(document.location.pathname);
+
+// let value;
+// value = (document.location.href);
+// console.log(value);
+
+// console.log(document.links.item(4).classList[3])
+
+// ------------------------------------------------
+
+const todoList = document.querySelector(".list-group");
+const card = document.getElementsByClassName("card");
+
+console.log(todoList)
+console.log(card)
 
 let value;
-value = (document.location.href);
-console.log(value);
 
-console.log(document.links.item(4).classList[3])
+value = todoList;
+value = todoList.children[1];
+value = todoList.children[todoList.children.length - 1].textContent = "Raze"
+value = Array.from(todoList.children);
+value.forEach(function (todo) {
+    console.log(todo.textContent);
+})
+console.log(value);
--- js/DOM2.js
+++ js/DOM2.js
@@ -1,28 +1,51 @@
-let h1 = document.getElementsByTagName("h1")
+let h1 = document.getElementsByTagName("h1");
 for (let i = 0; i < h1.length; i++) {
     console.log(h1[i])
 }
 
-// ---------------------------------------------
+// ------------------------------------------------------
 
-let num = document.getElementsByClassName('title1')
+let num = document.getElementsByClassName('title1');
 console.log(num)
 
-// ---------------------------------------------
+// ------------------------------------------------------
 
-let num2 = document.getElementById('title3')
-console.log(num2);
+let num2 = document.getElementById('title3');
+console.log(num2)
 
-// ---------------------------------------------
+// ------------------------------------------------------
 
 const h4 = document.getElementById('title4');
 h4.style.color = 'white';
 h4.style.fontSize = '40px';
-h4.title = 'selam'
+h4.title = 'selam';
+console.log(h4)
 
-// ---------------------------------------------
+// ------------------------------------------------------
 
-const h5=document.getElementById('title5');
-h5.setAttribute('style', 'font-size: 45px; color:red')
+const h5 = document.getElementById('title5');
+h5.setAttribute('style', 'font-size: 45px; color:red');
+console.log(h5)
 
-// ---------------------------------------------
+// ------------------------------------------------------
+
+const angara = document.getElementById('title5');
+angara.innerHTML = '<b>Başlık55</b>';
+console.log(angara)
+
+// -----------------------------------------------------
+
+const todoList= document.querySelector('#title3');
+todoList.setAttribute('style', 'color:yellow; font-size:20px');
+console.log(todoList)
+
+// -----------------------------------------------------
+
+const title2= document.querySelectorAll('.title1')[1];
+title2.setAttribute('style', 'color:pink; font-size:35px')
+console.log(title2)
+
+
+
+
+
