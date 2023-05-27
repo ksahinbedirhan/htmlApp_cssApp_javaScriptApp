55329d371e3fff4f496e79cd7a713969f570607e
ksahinbedirhan <ksahinbedirhan@gmail.com>
2023-05-27 16:35:18 +0300

    x

--- js/DOM1.js
+++ js/DOM1.js
@@ -9,5 +9,7 @@
 const clearButton = document.querySelector("#todoClearButton");
 clearButton.addEventListener("click", changeTitle);
 function changeTitle(e){
-    console.log(e.target.textContent)
+    console.log(e.target);
+    console.log(e.type)
 }
+
