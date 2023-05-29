c4f47adc4ca6a74018f0ef33d79c2650c957a8c6
ksahinbedirhan <ksahinbedirhan@gmail.com>
2023-05-29 10:57:19 +0300

    x

---
+++ kendi-uygulamalarım/bedirhanmobilya.js
@@ -0,0 +1,4 @@
+function bahçemobilyaları() {
+    target = "_blank";
+    window.open("https://www.google.com/search?q=bah%C3%A7e+mobilyalar%C4%B1&rlz=1C5GCEM_enTR1057TR1057&sxsrf=APwXEdcK9rGpHkGZF0MdyE2-K-JQYScEYw:1685197970309&source=lnms&tbm=isch&sa=X&ved=2ahUKEwii0ZXT25X_AhUTSvEDHUUpDtUQ_AUoAXoECAgQAw&biw=958&bih=969&dpr=1")
+}
